b282c3e36fbf6158b438884101bbfa56aa7547f5
jeremyocampo <jeremy_ocampojr@dlsu.edu.ph>
2019-08-07 02:50:35 +0800

    jermey changes stuff returnInv

--- resources/views/viewEventReturn.blade.php
+++ resources/views/viewEventReturn.blade.php
@@ -28,19 +28,74 @@
 		  
 			</div>
 		  </div>
-	<div class="card-body">
-		<div class="col-xl-10 mb-5 mb-xl-0">
-				<div class="card shadow">
-						<div class="card-header">
-							<div class="row">
-									<div class="col-xl-12 ">
-										<h2 calss="mb-0">View Item Details</h2>
+		
+				{{-- <div class="col-xl-8 mb-5 mb-xl-0"> --}}
+				<div class="col-xl-12 mb-5">
+					<div class="card shadow " >
+						<div class="card-header ">
+							<div class="row align-items-center">
+								<div class="col">
+									<div class="row">
+											<div class="col-md-5 ">
+													<h2 calss="mb-0">View Item Details</h2>
+												</div>
+									<div class="col-xs-2">
+											&nbsp;&nbsp;
 									</div>
+									
+									</div>
+								</div>
+								
+								<div class="col text-left">
+									{{-- <div class="row"> --}}
+										<div class="col-xs-5">
+									{{-- <input class="form-control" id="myInput" type="search" onkeyup="searchTable()" style="background: transparent;" placeholder="Search Item Here"> --}}
+									<input class="form-control" id="barcodeInput" type="number" onkeyup="checkBarcode()" style="background: transparent;" placeholder="Input Barcode Here" max=9999999999999>
+										</div>
+										{{-- <div class="col-xs-2">
+											&nbsp; &nbsp;
+										</div> --}}
+										{{-- <div class="col-xs-3">
+										<button type="button" class="btn btn-md btn-block" onclick="seachTable()">Search</button>
+										</div> --}}
+									{{-- </div> --}}
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									@if(session()->has('success'))
+										<br>
+										<div class="alert alert-success" role="alert">
+											<button type="button" data-dismiss="alert" class="close"><span aria-hidden="true">x</span></button>
+											{{ session()->get('success') }}<br>
+										</div>
+									@endif
+									@if(session()->has('deleted'))
+										<br>
+										<div class="alert alert-danger" role="alert">
+											<button type="button" data-dismiss="alert" class="close"><span aria-hidden="true">x</span></button>
+											{{ session()->get('deleted') }}<br>
+										</div>
+									@endif
+								</div>
+							</div>
+						   
+						</div>
+		  
+	<div class="card-body">
+		
+							<div class="row">
+									
 									<div class="col-xl-4">
 										{{-- <label class="text-muted">Event Name: {{$borrowedItems[0]->event_name}}</label> --}}
 										<label class="form-label">Event Name</label>
 										{{ Form::text('itemName', $borrowedItems[0]->event_name,['class' => 'form-control', 'disabled'] )}}
 									</div>
+									<div class="col-xl-4">
+											{{-- <label class="text-muted">Event Name: {{$borrowedItems[0]->event_name}}</label> --}}
+											<label class="form-label">Client Name</label>
+											{{ Form::text('itemName', $borrowedItems[0]->client_name,['class' => 'form-control', 'disabled'] )}}
+										</div>
 									<div class="col-xl-4">
 											<label class="form-label">Date Borrowed</label>
 											{{ Form::text('itemName', $borrowedItems[0]->event_start,['class' => 'form-control', 'disabled'] )}}
@@ -75,56 +130,47 @@
                                     <thead class="thead-light">
                                         <tr>
 											<th scope="col">Item Name</th>
+											<th scope="col">Color</th>
 											<th scope="col">Quantity</th>
-                                            <th scope="col">Venue</th>
-                                            <th scope="col">Borrow Date/Time</th>
-                                            <th scope="col">Return Date/Time</th>
-                                            <th scope="col">Status</th>
-                                            <th scope="col">Action</th>
+											<th scope="col">Event Barcode</th>
+											<th scope="col">Rental Cost</th>
+											<th scope="col">Total Rental Cost</th>
+                                            <th scope="col">Items Scanned</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($borrowedItems as $i)
                                         @if($i->status > 0)
                                         <tr>
-                                            <td>
-												
-												
+                                            
+											<td>{{ $i->inventory_name }}</td>
+											<td>{{ $i->color_name }}</td>
+                                            <td>{{ $i->qty }}</td>
+											<td>
+												<div id="barcode-{!! $i->inventory_id !!}" value="{!! "toPrint-" . $i->inventory_id!!}">
+													{{-- <a href="" class="dropdown-item" onclick="printContent('barcode-{{$i->inventory_id}}');" id="printBtn{{ $i->inventory_id }}"> --}}
+														{!!'<img src="data:image/png;base64,' . DNS1D::getBarcodePNG("".$i->esku, "C128A",2,44,array(1,1,1), true) . '" alt="barcode"   />' !!}
+													{{-- </a> --}}
+												</div>	
 											</td>
-                                            <td>{{ $i->venue }}</td>
-                                            <td>{{ $i->event_start }}</td>
-                                            <td>{{ $i->event_end }}</td>
-                                            <td>{{ $i->status_name}} </td>
-                                            <td>
+											<td>{{ $i->rental_cost}} </td>
+											<td>{{ $i->rent_price}} </td>
+											<td>
+												<div class="col-xl-4">
+													<label class="form-label">Qty to Return</label>
+													<input type="text" disabled class="form-control" name="qtyReturned" id="qtyReturn{{ $i->einventory_id}}">
+												</div>
+											</td>
+											
+
+                                            {{-- <td>
                                                 <a class="" href="{{ url('returnInventory/'.$i->event_id) }}" >
                                                     
                                                     <button class="btn btn-block btn-sm"><i class="ni ni-zoom-split-in"></i> &nbsp; View Event Details</button>
-                                                    {{-- <span>{{ __('View Event Details') }}</span> --}}
+                                                    
                                                 </a>
-                                                {{-- <div class="dropdown">
-                                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                        Action
-                                                    </button>
-                                                    <div class="dropdown-menu dropdown-menu-arrow dropdown-menu">
-                                                        <div class=" dropdown-header noti-title">
-                                                            <h6 class="text-overflow m-0">{{ __('Please Select an Action!') }}</h6>
-                                                        </div>
-                                                        <div class="dropdown-divider"></div>
-                                                        <a href="{{ url('returnInventory/'.$i->event_id) }}" class="dropdown-item">
-                                                            <i class="ni ni-zoom-split-in"></i>
-                                                            <span>{{ __('View Event Details') }}</span>
-                                                        </a>
-                                                        <a href="" class="dropdown-item" onclick="event.preventDefault();
-                                                            document.getElementById('delete-form-{{ $i->event_name }}').submit();">
-                                                            <i class="ni ni-fat-remove"></i>
-                                                            <span>{{ __('Remove from Inventory') }}</span>
-                                                            {!! Form::open(['action' => ['InventoryController@destroy', $i->event_name], 'method' => 'POST', 'id' => 'delete-form-'.$i->inventory_id]) !!}
-                                                                {{ Form::hidden('_method','DELETE')}}
-                                                            {!! Form::close() !!}
-                                                        </a>
-                                                    </div>
-                                                </div> --}}
-                                            </td>
+                                                
+                                            </td> --}}
                                         </tr>
                                         
                                         @endif
@@ -135,7 +181,7 @@
 				</div>
 				<div class="card-footer text-muted">
 						<div class="text-right">
-								<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Save</button>
+								<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Return Items to Inventory</button>
 								<a href="{{ url('returnInventory')}}" class="btn btn-default">Back</a>
 								{{-- <a href="" class="btn btn-primary" onclick="printContent('barcode-{{ $itemInfo[0]->inventory_id }}');" id="printBtn{{ $itemInfo[0]->inventory_id}}">
 									<i class="ni ni-single-copy-04"></i>
